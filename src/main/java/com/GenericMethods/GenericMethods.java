5168143a6b6fae3cfcdf54822eefdd1cec7f89af
ranjithkumar <knsrk45@gmail.com>
2019-06-26 17:49:52 +0530

    IsDisplayed method is added to genric method

--- src/main/java/com/GenericMethods/GenericMethods.java
+++ src/main/java/com/GenericMethods/GenericMethods.java
@@ -240,7 +240,41 @@ public class GenericMethods extends ExtentReports_Reporting {
 		return status;
 	}
 	
+	/**
+	 @name:IsDisplayed
+	 @Purpose:This method is used for is Element successfully displayed on the application
+	 @Input:WebElement element,String elementname,String Pagename
+	 @Output:boolean
+	 @author:ranjith
+	 @DesignDate:06/26/2019
+	 **/
 	
+	public boolean IsDisplayed(WebElement element,String elementname,String Pagename)
+	{
+		boolean status = true;
+		
+		try
+		{
+			boolean isDisplayeStatus = element.isDisplayed();
+			if(isDisplayeStatus)
+			{
+				System.out.println("The element"+elementname+ "is successfully displayed on page"+Pagename);
+			}
+			else
+			{
+				System.out.println("The element"+elementname+ "is not successfully displayed on page"+Pagename);
+				System.out.println("unable to find the element"+elementname);
+				status=false;
+			}
+		}
+		catch(Exception e)
+		{
+		System.out.println(e.getMessage());
+			
+		}
+		return status;
+	}
+
 	
 
 }
